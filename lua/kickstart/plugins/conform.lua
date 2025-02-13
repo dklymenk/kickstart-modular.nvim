5fc21d3a45c1e97f1dfd093504956054d7f5fcbd
Dmytro Klymenko <dmytro.klymenko@syniti.com>
2025-02-13 09:07:37 -0500

    Run prettier in markdown

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -64,6 +64,7 @@ return {
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { 'prettierd' },
+        markdown = { 'prettierd' },
       },
     },
   },
