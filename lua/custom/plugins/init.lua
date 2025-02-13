1ecc1fedfad13e518e16cca5d0459077ade1d2fe
Dmytro Klymenko <dmytro.klymenko@syniti.com>
2025-02-13 09:07:53 -0500

    Add markdown preview update plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -28,4 +28,14 @@ return {
   },
   { 'ruanyl/vim-gh-line' },
   { 'chaoren/vim-wordmotion' },
+  {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup()
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
 }
