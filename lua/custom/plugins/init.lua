7f7329c408343c378e57f050b9a3f93330a5cd65
Dmytro Klymenko <dmytro@klymenko.xyz>
2026-09-18 18:08:54 -0400

    Add go.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -140,4 +140,31 @@ return {
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
+  {
+    'ray-x/go.nvim',
+    tag = 'v0.11',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      -- { "nvim-treesitter/nvim-treesitter", branch = 'main' } -- optional for master version
+    },
+    opts = function()
+      require('go').setup(opts)
+      local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.go',
+        callback = function()
+          require('go.format').goimports()
+        end,
+        group = format_sync_grp,
+      })
+      return {
+        -- lsp_keymaps = false,
+        -- other options
+      }
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
